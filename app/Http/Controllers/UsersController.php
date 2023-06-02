d9135632e6b7dd7308e35cb5c9d44dd9dbd75e1a
Milka Wanga <bakhuyamilka@gmail.com>
2023-06-02 16:47:11 +0300

    Created an if condition to check if user is assigned

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,6 +8,6 @@ class UsersController extends Controller
 {
     public function workLoad()
     {
-        return view("viewUsers", ['viewUsers'=>['Janet','Marion','Brian']]);
+        return view("viewUsers", ['viewUsers'=>['Janet']]);
     }
 }
--- resources/views/viewUsers.blade.php
+++ resources/views/viewUsers.blade.php
@@ -1,1 +1,7 @@
-<h1>    View Users    {{count($viewUsers)}}</h1>
+ <h1>    View Users   {{-- {{count($viewUsers)}}--}}</h1> 
+ {{-- if condition --}}
+ @if($viewUsers=='Janet')
+ <h3>Hi{{$viewUsers}}</h3>
+ {{-- @elseif($viewUser=='Janet')
+ <h3>Hi{{$viewUser}}</h3> --}}
+ @endif
