994e0269a55d35d135d1c2f5b006170ffaa49b46
Milka Wanga <bakhuyamilka@gmail.com>
2023-06-03 10:27:55 +0300

    Put error message to appear below the input field

--- resources/views/formView.blade.php
+++ resources/views/formView.blade.php
@@ -2,14 +2,17 @@
 {{-- Action is the path in route --}}
 {{-- show error validatio --}}
 {{-- {{$errors}} --}}
-@if($errors->any())
+{{-- error message when field is emmpty --}}
+{{-- @if($errors->any())
 @foreach ($errors->all() as $err)
     <li>{{$err}}</li>
 @endforeach
-@endif
+@endif --}}
+{{-- Make error appear in its field --}}
 <form action="formView" method="POST">
     @csrf
-<input type="text" name="username" placeholder="username"><br><br>
+<input type="text" name="username" placeholder="username"><br>
+@error('username'){{$message}}@enderror<br>
 <input type="password" name="password" placeholder="password"><br><br>
 
 <input type="submit" value="Submit">
