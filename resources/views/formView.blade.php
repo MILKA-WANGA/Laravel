1e96c4e97a34e4c0fdadc5e1bb63be5099893427
Milka Wanga <bakhuyamilka@gmail.com>
2023-06-03 09:25:18 +0300

    Created a form in formView.blade.php file

---
+++ resources/views/formView.blade.php
@@ -0,0 +1,6 @@
+<h1>Form Controller</h1>
+{{-- Action is the path in route --}}
+<form action="formView" method="POST">
+<input type="text" name="username" placeholder="username"><br><br>
+<input type="button" value="Submit">
+</form>
