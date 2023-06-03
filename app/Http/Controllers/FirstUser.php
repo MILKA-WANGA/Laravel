f43918ce17fe558db776aa23d2f7fb54a04b4b30
Milka Wanga <bakhuyamilka@gmail.com>
2023-06-03 20:36:21 +0300

    Configure the database:lara in .env file

--- app/Http/Controllers/FirstUser.php
+++ app/Http/Controllers/FirstUser.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+//Connect to DB
+use Illuminate\Support\Facades\DB;
 
 class FirstUser extends Controller
 {
     public function index()
     {
-        return "Hello from controller";
+        return DB::select("select * from user");
+        // return "Hello from controller";
     }
 }
