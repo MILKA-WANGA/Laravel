f27d8fe847506cd9a0b8f7711c086c223684ea45
Milka Wanga <bakhuyamilka@gmail.com>
2023-06-03 09:45:49 +0300

    Added cross-site request form(csrf) in form

--- resources/views/formView.blade.php
+++ resources/views/formView.blade.php
@@ -1,6 +1,7 @@
 <h1>Form Controller</h1>
 {{-- Action is the path in route --}}
 <form action="formView" method="POST">
+    @csrf
 <input type="text" name="username" placeholder="username"><br><br>
 <input type="submit" value="Submit">
 </form>
