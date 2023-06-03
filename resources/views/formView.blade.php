6cc272958b9fbf3611542ec8fd3c988d3ff6da6d
Milka Wanga <bakhuyamilka@gmail.com>
2023-06-03 10:18:53 +0300

    Changed the error message

--- resources/views/formView.blade.php
+++ resources/views/formView.blade.php
@@ -1,7 +1,12 @@
 <h1>Form Controller</h1>
 {{-- Action is the path in route --}}
 {{-- show error validatio --}}
-{{$errors}}
+{{-- {{$errors}} --}}
+@if($errors->any())
+@foreach ($errors->all() as $err)
+    <li>{{$err}}</li>
+@endforeach
+@endif
 <form action="formView" method="POST">
     @csrf
 <input type="text" name="username" placeholder="username"><br><br>
