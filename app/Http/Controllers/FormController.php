88cf764cfe71fc31d5f2deabe122fb4f6530976c
Milka Wanga <bakhuyamilka@gmail.com>
2023-06-03 10:06:52 +0300

    Validate the form:required

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -9,6 +9,12 @@ class FormController extends Controller
     //get  form input data
     public function getData(Request $req)
     {
+        // return $req->input();
+        // Validate fields
+        $req->validate([
+            'username'=>'required',
+            'password'=>'required'
+        ]);
         return $req->input();
     }
 }
--- resources/views/formView.blade.php
+++ resources/views/formView.blade.php
@@ -1,7 +1,11 @@
 <h1>Form Controller</h1>
 {{-- Action is the path in route --}}
+{{-- show error validatio --}}
+{{$errors}}
 <form action="formView" method="POST">
     @csrf
 <input type="text" name="username" placeholder="username"><br><br>
+<input type="password" name="password" placeholder="password"><br><br>
+
 <input type="submit" value="Submit">
 </form>
