d74483835e7b54d1ff515664b735159be207e09e
Milka Wanga <bakhuyamilka@gmail.com>
2023-06-04 11:04:14 +0300

    Used method:DELETE in loginView.blade.php

--- resources/views/loginView.blade.php
+++ resources/views/loginView.blade.php
@@ -3,7 +3,7 @@
 
 <form  action="loginView" method="POST">
     {{-- Using put and delete methods  use method post and add the following code--}}
-    {{method_field('PUT')}}
+    {{method_field('DELETE')}}
     @csrf
     <input type="text" name="username" placeholder="Username">
     <input type="password" name="password" placeholder="Password">
