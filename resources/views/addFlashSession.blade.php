ebb02a95d9ef5f5475ab03c89d438a4751fcf7aa
Milka Wanga <bakhuyamilka@gmail.com>
2023-06-04 15:32:05 +0300

    Created a Cotroller:AddFlashController.php

--- resources/views/addFlashSession.blade.php
+++ resources/views/addFlashSession.blade.php
@@ -4,8 +4,8 @@
     {{-- Using put and delete methods  use method post and add the following code--}}
     {{-- {{method_field('DELETE')}} --}}
     @csrf
-    <input type="text" name="username" placeholder="Username">
-    <input type="password" name="password" placeholder="Password">
-    <input type="email" name="email" placeholder="Email">
+    <input type="text" name="username" placeholder="Username"><br><br>
+    <input type="password" name="password" placeholder="Password"><br><br>
+    <input type="email" name="email" placeholder="Email"><br><br>
      <input type="button" value="button">
 </form>
