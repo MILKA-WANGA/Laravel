2148e221d260848f980d6ce35f9a3291712835fb
Milka Wanga <bakhuyamilka@gmail.com>
2023-06-04 17:08:34 +0300

    Fixed:Replaced method post with view for route

--- resources/views/UploadFile.blade.php
+++ resources/views/UploadFile.blade.php
@@ -1,5 +1,5 @@
 <h3>Upload File</h3>
-<form action="" enctype="multipart/form-data">
+<form action="uploadFile" method="POST" enctype="multipart/form-data">
     <input type="file" name="file" id="file" >
     <button type="submit">Upload</button>
 
--- routes/web.php
+++ routes/web.php
@@ -92,7 +92,7 @@ return redirect("log");
 });
 Route::view("addFlashSession","addFlashSession");
 Route::post("addMember",[AddFlashController::class,'add']);
-Route::post("upload","uploadFile");
+Route::view("upload","uploadFile");
 
 
 
