99c93a550f75076f49c458748d246e916e02e30a
Milka Wanga <bakhuyamilka@gmail.com>
2023-06-04 10:17:55 +0300

    Created a  Controller:LoginController

--- routes/web.php
+++ routes/web.php
@@ -56,6 +56,7 @@ Route::view("inner","inner")->Middleware("ageProtects");
 Route::get("firstUser",[FirstUser::class,'index']);
 Route::get("users",[UserController::class,'getData']);
 Route::get("hello",[UserApiController::class,'index']);
+Route::view("logins","loginView");
 
 
 
