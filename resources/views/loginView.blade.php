41c5a117baa2798052df5e8b591204922d106a64
Milka Wanga <bakhuyamilka@gmail.com>
2023-06-04 10:50:23 +0300

    Fixed: Page expired

--- resources/views/loginView.blade.php
+++ resources/views/loginView.blade.php
@@ -1,5 +1,8 @@
+
 <h3>Login Form</h3>
+
 <form  action="loginView" method="POST">
+    @csrf
     <input type="text" name="username" placeholder="Username">
     <input type="password" name="password" placeholder="Password">
      <input type="submit" value="submit">
