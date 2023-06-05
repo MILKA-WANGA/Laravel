6898aa0a47fa0364728d41340ab72e5c1fe734a2
Milka Wanga <bakhuyamilka@gmail.com>
2023-06-05 10:52:44 +0300

    Fetched data from table:members

--- app/Http/Controllers/ListController.php
+++ app/Http/Controllers/ListController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Member;
 
 class ListController extends Controller
 {
     //
     function index()
     {
-        return ("usersList");
+        return Member::all();
     }
 }
