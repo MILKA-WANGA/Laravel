2f6c275c606c02fa0dee223d95add7e405c62cb0
Milka Wanga <bakhuyamilka@gmail.com>
2023-06-07 14:51:21 +0300

    Fetched data from table:add_members

--- app/Http/Controllers/MembersController.php
+++ app/Http/Controllers/MembersController.php
@@ -3,15 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-//use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\DB;
 
 class MembersController extends Controller
 {
     //
    public  function dbOperations()
     {
-        // return DB::table('add_members')-
-        return view("membersListed");
+        return DB::table('add_members')->get();
+        // return view("membersListed");
     }
 }
 ?>
