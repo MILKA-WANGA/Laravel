faace7f27651c84c53d968c3af38e3981a9d8524
Milka Wanga <bakhuyamilka@gmail.com>
2023-06-07 23:28:00 +0300

    Inserted data into the database

--- app/Http/Controllers/MembersController.php
+++ app/Http/Controllers/MembersController.php
@@ -9,8 +9,17 @@ class MembersController extends Controller
 {
     //
    public  function dbOperations()
-    {
-        return (array)DB::table('add_members')->find(2);
+    {    
+        return (array)DB::table('add_members')->insert([
+        'name'=>'name',
+        'email'=>'email',
+        'password'=>'password',
+        'updated_at'=>'updated_at',
+        'created_at'=>'created_at'
+    ]);
+        //return (array)DB::table('add_members')->find(2);
+        //Get the number of records
+        //return DB::table('add_members')->count();
        // return DB::table('add_members')->where ('id',1)->get();
         
         //return view("membersListed",['data'=>$data]);
