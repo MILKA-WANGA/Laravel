ddd6e4040b4ce4cbd85009e6783f9bebdf34d9e0
Milka Wanga <bakhuyamilka@gmail.com>
2023-06-07 15:04:52 +0300

    Passed fetched data in the view:membersListed.blade.php

--- resources/views/membersListed.blade.php
+++ resources/views/membersListed.blade.php
@@ -1,6 +1,24 @@
 <h1>Query Builder</h1>
 <table>
     <tr>
-        <td></td>
+        <td>Number</td>
+        <td>Name</td>
+        <td>Email</td>
+        <td>Password</td>
+        <td>Date Update</td>
+        <td>Date Created</td>
     </tr>
+    
+        @foreach ($data as $item)
+        <tr>
+            <td>{{$item[id]}}</td>
+            <td>{{$item[name]}}</td>
+            <td>{{$item[email]}}</td>
+            <td>{{$item[Password]}}</td>
+            <td>{{$item{updated_at}}}</td>
+            <td>{{$item[created_at]}}</td>
+        </tr>
+            
+        @endforeach
+    
 </table>
