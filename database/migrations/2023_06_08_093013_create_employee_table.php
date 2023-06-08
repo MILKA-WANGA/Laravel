3c193b8a78f723fdc5f85512317de465aff2dae5
Milka Wanga <bakhuyamilka@gmail.com>
2023-06-08 12:31:51 +0300

    Fixed:Bad method while creating employee table

--- database/migrations/2023_06_08_093013_create_employee_table.php
+++ database/migrations/2023_06_08_093013_create_employee_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateEmployeesTable extends Migration
+class CreateEmployeeTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,8 @@ class CreateEmployeesTable extends Migration
      */
     public function up()
     {
-        Schema::create('employees', function (Blueprint $table) {
+        Schema::create('employee', function (Blueprint $table) {
             $table->id();
-            $table->Sting('name');
-            $table->String('email');
-            $table->String('password');
-            $table->String('Contact');
             $table->timestamps();
         });
     }
@@ -30,6 +26,6 @@ class CreateEmployeesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('employees');
+        Schema::dropIfExists('employee');
     }
 }
