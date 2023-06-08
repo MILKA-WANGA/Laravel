aea77d5738e8c500e780a53559a57e50a8d7908d
Milka Wanga <bakhuyamilka@gmail.com>
2023-06-08 12:27:13 +0300

    Created table:employees

--- database/migrations/2023_06_08_092441_create_employees_table.php
+++ database/migrations/2023_06_08_092441_create_employees_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTestTable extends Migration
+class CreateEmployeesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,12 @@ class CreateTestTable extends Migration
      */
     public function up()
     {
-        Schema::create('test', function (Blueprint $table) {
+        Schema::create('employees', function (Blueprint $table) {
             $table->id();
-            $table->String ('name');
-            $table->String ('email"');
-            $table->string('password');
+            $table->Sting('name');
+            $table->String('email');
+            $table->String('password');
+            $table->String('Contact');
             $table->timestamps();
         });
     }
@@ -29,6 +30,6 @@ class CreateTestTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('test');
+        Schema::dropIfExists('employees');
     }
 }
