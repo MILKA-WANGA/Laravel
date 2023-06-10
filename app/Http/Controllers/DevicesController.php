9a59a7349fc16525098df8c3d9ea2a409a58f7d4
Milka Wanga <bakhuyamilka@gmail.com>
2023-06-10 22:26:56 +0300

    Imported Model:Device in Controller:DevicesController

--- app/Http/Controllers/DevicesController.php
+++ app/Http/Controllers/DevicesController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Device;
 
 class DevicesController extends Controller
 {
     //
     function index()
     {
-        
+     return Device::all();
     }
 }
