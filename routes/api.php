f59f91a19c8e77ed2589aea449876bf13cff835b
Milka Wanga <bakhuyamilka@gmail.com>
2023-06-10 22:24:58 +0300

    Created an api route for Controller:DevicesController

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\Devices;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MyFirst;
+use App\Http\Controllers\DevicesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,3 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::get("data",[MyFirst::class,'index']);
+Route::get("list",[DevicesController::class,'index']);
